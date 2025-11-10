14508ddad640b042b2f79b0539e66a5a40f964f3
Your Name <you@example.com>
2025-11-10 21:41:31 +0100

    Boids&balls Sim

--- multiagents_systems_simulation/src/main/BallsSimulator.java
+++ multiagents_systems_simulation/src/main/BallsSimulator.java
@@ -13,6 +13,7 @@ public class BallsSimulator implements Simulable {
     private int width;
     private int height;
     int radius = 7;
+    EventManager manager;
 
 
     // Constructor
@@ -22,12 +23,38 @@ public class BallsSimulator implements Simulable {
         this.width = guis.getWidth();
         this.height = guis.getHeight();
         this.colors = colors;
-        
+        this.manager = new EventManager();
+        this.manager.addEvent(new EventBalls(0, this.balls, this, this.manager));
         this.reDisplay();
     }
     
     @Override
     public void next(){
+        manager.next();
+    }
+
+
+    @Override
+    public void restart(){
+        balls.reInit();
+        manager.restart();
+        manager.addEvent(new EventBalls(0, this.balls, this, this.manager));
+        this.reDisplay();
+    }
+
+    public void reDisplay(){
+        this.guis.reset();
+        List<Point> points = this.balls.getBalls();
+        Color[] colors = this.colors;
+        for (int i = 0; i < points.size(); i++){
+            Color color = colors[i % colors.length];
+            Point p = points.get(i);
+            gui.Oval ball = new gui.Oval(p.x, p.y, color, color, 2*this.radius, 2*this.radius);
+            guis.addGraphicalElement(ball);
+        }
+
+    }
+    public void moveBalls(){
         List<Point> points = balls.getBalls();
         List<Point> velocities = balls.getVelocities();
         // Update dimensions in case of window resize
@@ -49,30 +76,11 @@ public class BallsSimulator implements Simulable {
             }
 
             // Bounce vertically
-            if (p.y < 0 || p.y + 2*this.radius > height) {
+            if (p.y < 1 || p.y + 2*this.radius > height) {
                 v.y = -v.y;
                 p.y = Math.max(0, Math.min(p.y, height - 2*this.radius));
             }
         }
-        this.reDisplay();
     }
 
-    @Override
-    public void restart(){
-        balls.reInit();
-        this.reDisplay();
-    }
-
-    public void reDisplay(){
-        this.guis.reset();
-        List<Point> points = this.balls.getBalls();
-        Color[] colors = this.colors;
-        for (int i = 0; i < points.size(); i++){
-            Color color = colors[i % colors.length];
-            Point p = points.get(i);
-            gui.Oval ball = new gui.Oval(p.x, p.y, color, color, 2*this.radius, 2*this.radius);
-            guis.addGraphicalElement(ball);
-        }
-
-    }
 }
