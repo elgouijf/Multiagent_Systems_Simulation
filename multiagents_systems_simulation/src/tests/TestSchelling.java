285440c1df4e6ad74e3bc31ab9a046ad62d040c3
AybTouati <ayoubtouati1203@gmail.com>
2025-11-15 10:07:00 +0100

    modification

--- multiagents_systems_simulation/src/tests/TestSchelling.java
+++ multiagents_systems_simulation/src/tests/TestSchelling.java
@@ -1,9 +1,12 @@
+package tests;
+import main.*;
+
 import gui.GUISimulator;
 import java.awt.Color;
 
 public class TestSchelling {
     public static void main(String[] args) {
-        GUISimulator gui = new GUISimulator(500, 500, Color.BLACK);
+        GUISimulator gui = new GUISimulator(600, 600, Color.BLACK);
         Schelling model = new Schelling(50, 2, 3); // grille 50x50, 2 couleurs, seuil K=3
         new SchellingSimulator(gui, model);
     }
